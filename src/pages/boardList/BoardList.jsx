1f4a937f00b8a8f090236e8a2667d948e21dadff
BeLeap <changseo_jang@korea.ac.kr>
2020-08-01 00:59:04 +0900

    ls 명령어 권한 등 상세정보 추가

--- src/pages/boardList/BoardList.jsx
+++ src/pages/boardList/BoardList.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 
-const BoardList = () => (
-  <>
-    <div>
-      notice
-    </div>
-    <div>
-      board
-    </div>
-  </>
-);
+const BoardList = () => {
+  const month = [];
+  month[0] = 'Jan';
+  month[1] = 'Feb';
+  month[2] = 'Mar';
+  month[3] = 'Apr';
+  month[4] = 'May';
+  month[5] = 'Jun';
+  month[6] = 'Jul';
+  month[7] = 'Aug';
+  month[8] = 'Sep';
+  month[9] = 'Oct';
+  month[10] = 'Nov';
+  month[11] = 'Dec';
+
+  const today = new Date();
+  const date = today.getDate();
+  const mon = month[today.getMonth()];
+  const time = `${(new Date()).getHours().toString()}:${(new Date()).getMinutes().toString()}`;
+
+  const todayString = `${date} ${mon} ${time}`;
+
+  return (
+    <>
+      <div>
+        drwxrwx--- - kuics kuics
+        {' '}
+        {todayString}
+        {' '}
+        notice
+      </div>
+      <div>
+        drwxrwx--- - kuics kuics
+        {' '}
+        {todayString}
+        {' '}
+        board
+      </div>
+    </>
+  );
+};
 
 export default BoardList;
