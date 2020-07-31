7757a6b55d8dfdb700dfcff7c0f25580e35abb18
BeLeap <changseo_jang@korea.ac.kr>
2020-07-31 21:45:56 +0900

    반응형

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 import kuicsNotFound from '../../images/KUICS-404.png';
+import Command from '../../components/common/Command';
 
 const NotFound = () => (
-  <img src={kuicsNotFound} alt="Not Found" />
+  <>
+    <div>
+      Unknown Command
+    </div>
+    <div>
+      type
+      {' '}
+      <Command>help</Command>
+      {' '}
+      to see command list
+    </div>
+  </>
 );
 
 export default NotFound;
